6ecbdea38eaba003d0512685a19846fc61d0b3ff
Aya AbdulSlam <ahemaid@integrant.com>
2021-05-17 03:25:06 +0200

    Initial Create

---
+++ RulesDesignPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using Core.StoreExample;
+using System;
+
+namespace RulesDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DiscountCalculator calc = new DiscountCalculator();
+            Customer cust = new Customer
+            {
+                DateOfBirth = new DateTime(1993, 2, 20),
+                DateOfFirstPurchase = new DateTime(2021, 2, 20),
+                IsVeteran = true
+            };
+            Console.WriteLine($"Discount for the Customer: {calc.CalculateDiscountPercentage(cust)}");
+        }
+    }
+}
